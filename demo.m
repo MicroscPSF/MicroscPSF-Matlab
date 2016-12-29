9266d6b1a3a17f45de7ecae41fe582c19b31b193
Jizhou Li <hijizhou@users.noreply.github.com>
2016-12-29 14:08:18 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,8 +1,16 @@
 %% Demo for the MicroPSF, a super-fast and accurate approximation of the Gibson-Lanni model
 % If the parameters are not assigned, default setting will be loaded, see
 % MicroPSF for details.
-
-%   Copyright © Jizhou Li, 2016, The Chinese University of Hong Kong
+%
+%   Reference:
+%       [1] Gibson, S.F. & Lanni, F., 1992.
+%           Experimental test of an analytical model of aberration in an
+%           oil-immersion objective lens used in three-dimensional light
+%           microscopy. JOSA A, 9(1), pp.154-166.
+%       [2] Li, J., Luisier, F. and Blu, T. Fast and accurate 3D PSF
+%           computation in fluorescence microscopy. JOSA A. Submitted.
+%
+%   Copyright Â© Jizhou Li, 2016, The Chinese University of Hong Kong
 %   Update date: 28 Dec, 2016
 
 addpath('Utilities/');
